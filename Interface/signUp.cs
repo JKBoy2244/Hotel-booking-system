f8b368402077b6e02b8771cdc7f94627fe6332b1
JKBoy2244 <jobayer2006@gmail.com>
2026-01-03 23:15:09 +0000

    Create signUp.cs

---
+++ Interface/signUp.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+
+interface signUp {
+
+  public void firstName();
+  public void lastName();
+  public void age();
+  public void emailAddress();
+  public void passWord();
+  public void confirmPassword();
+
+}
