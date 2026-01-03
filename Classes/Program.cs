ba423263118a3e11af7c77e66725f14cb32ef974
JKBoy2244 <jobayer2006@gmail.com>
2026-01-03 23:17:45 +0000

    Create Program.cs

---
+++ Classes/Program.cs
@@ -0,0 +1,15 @@
+class Program {
+  public static void Main (string[] args) {
+
+    confirmCreateAccount user = new confirmCreateAccount("","",0,"","","");
+    user.firstName();
+    user.lastName();
+    user.age();
+    user.emailAddress();
+    user.passWord();
+    user.confirmPassword();
+    user.display();
+    user.confirm();
+    
+  }
+}
