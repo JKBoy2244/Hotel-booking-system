c8f9556f3c6f5389c59e5a6d0ea2f7e3e249be0b
JKBoy2244 <jobayer2006@gmail.com>
2026-01-03 23:14:38 +0000

    Create Login.cs

---
+++ Interface/Login.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+
+
+interface Login {
+
+  public void emailAddress();
+  public void passWord();
+
+}
