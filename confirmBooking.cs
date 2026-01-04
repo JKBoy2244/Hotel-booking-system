9c4a81954813475b810138042eb3292703dbe372
JKBoy2244 <jobayer2006@gmail.com>
2026-01-04 23:55:29 +0000

    Create confirmBooking.cs

---
+++ confirmBooking.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class confirmBooking : roomBooking {
+
+  public confirmBooking(String roomType, String roomNumber, String checkInDate, String checkOutDate, int numberOfPeople) 
+    : base(roomType, roomNumber, checkInDate, checkOutDate, numberOfPeople) { }
+
+
+   public void display() {
+     for (int i = 0; i < roomTypes.Count; i++) {
+
+         Console.WriteLine($"{roomTypes[i]} {roomNumbers[i]} {checkInDates[i]} {checkOutDates[i]} {NumbersOfPeople[i]}");
+     }
+  }
