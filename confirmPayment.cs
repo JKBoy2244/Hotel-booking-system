fb3d23477acb17d443e40149c783cbf552db8cb6
JKBoy2244 <jobayer2006@gmail.com>
2026-01-05 00:37:36 +0000

    Create confirmPayment.cs

---
+++ confirmPayment.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+
+class confirmPayment : Payment {
+
+  public void confirm() {
+    Console.WriteLine("Payment confirmed - Step 3 complete!");
+    Console.WriteLine("Thank you for booking with us. Please take a seat and we will call you in a while!");
+    Console.WriteLine("Have a good day!");
+  }
+}
