d69c43cfe1e5ac46a545e250a8e3f3d5d2b2c0a0
JKBoy2244 <jobayer2006@gmail.com>
2026-01-05 14:43:20 +0000

    Create login.cs

---
+++ login.cs
@@ -0,0 +1,25 @@
+class login {
+
+  public void loginUser(List<string> emailAddresses, List<string> passWords) {
+    
+    Console.WriteLine("Email address: ");
+    String email = Console.ReadLine();
+    Console.WriteLine("Password: ");
+    String password = Console.ReadLine();
+
+    bool found = false;
+    for (int i = 0; i < emailAddresses.Count; i++) {
+      if (email != null && password != null && emailAddresses[i].Equals(passWords[i])) {
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) {
+      Console.WriteLine("Sorry that's incorrect, try again!");
+      return;
+    }
+
+    Console.WriteLine("Login successful!");
+  }
+}
