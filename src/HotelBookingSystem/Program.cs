aace4968eaa1fe2cc892b17e3cc04f529132acad
JKBoy2244 <jobayer2006@gmail.com>
2026-02-01 12:13:22 +0000

    Update Program.cs

--- src/HotelBookingSystem/Program.cs
+++ src/HotelBookingSystem/Program.cs
@@ -1,9 +1,9 @@
-namespace HotelBookingSystem;
-
 using System;
 using System.Linq;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+
+namespace HotelBookingSystem;
 
 class Program {
   public static void Main (string[] args) {
