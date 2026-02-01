c2da087b31a4bc0698958c064313e14d9629a9ea
JKBoy2244 <jobayer2006@gmail.com>
2026-02-01 11:34:16 +0000

    Update createAccount.cs

--- src/HotelBookingSystem/createAccount.cs
+++ src/HotelBookingSystem/createAccount.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
-abstract class createAccount : signUp {
+abstract class createAccount : ISignUp {
 
   private String _firstName;
   private String _lastName;
